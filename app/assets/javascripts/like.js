1abaa55a897ec7a6ab655f240f339b46343a4de3
sashadev-sky <sboginsky17@gmail.com>
2019-02-18 08:14:23 -0500

    bootstrap workaronud attempt

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -2,57 +2,101 @@
 
 // functionalize appearance changes
 function changelikecount(value, node_id) {
+  window.value = value;
+  window.node_id = node_id;
 
   var count = $('#like-count-' + node_id).html();
+  window.count = count;
   // strip parens and convert to number
   count = parseInt(count);
   count += value;
+  window.count2 = count;
   // push value back out
   $('#like-count-' + node_id).html(count);
 
 }
 
-function shownotliked(node_id) {
+// function shownotliked(node_id) {
 
-  $('#like-star-' + node_id)[0].className = "fa fa-star-o";
+  // $('#like-star-' + node_id)[0].className = "fa fa-star-o";
 
+// }
+
+// function showliked(node_id) {
+
+//   $('#like-star-' + node_id)[0].className = "fa fa-star";
+
+// }
+
+function renderLikeStar(value, node_id) {
+  let name;
+  value === -1 ? name = "fa fa-star" : name = "fa fa-star-o"
+  $('#like-star-' + node_id)[0].className = name;
 }
 
-function showliked(node_id) {
 
-  $('#like-star-' + node_id)[0].className = "fa fa-star";
-
+function changeLikeStatus(node_id, method) {
+  // method should be passed in
+  let msg;
+  method === "/delete" ? msg = "Unliked!" : msg = "Liked!";
+  window.msg = msg;
+  console.log(window.msg);
+  $.getJSON("/likes/node/" + node_id + `${method}`).then(function(response) {
+    notyNotification('mint', 3000, 'success', 'topRight', `${msg}`);
+    changelikecount(parseInt(response), node_id);
+    renderLikeStar(parseInt(response), node_id);
+    // $(this).prop('disabled', false);
+  }).then(function(response) {
+    let method1 = method === "/delete" ? clickliked : clicknotliked
+    let method2 = method1 === clickliked ? clicknotliked : clickliked
+    $('#like-button-' + node_id).on('click', method1);
+    return $('#like-button-' + node_id).prop('disabled', false)
+  });
+  
 }
 
 // support AJAX button clicking
+function clicknotliked() {
+  var node_id = $(this).attr('node-id');
+  $('#like-button-' + node_id).prop('disabled', true)
+  $('#like-button-' + node_id).off('click', clickliked);
+  $('#like-button-' + node_id).off('click', clicknotliked);
+  // notyNotification('mint', 3000, 'success', 'topRight', 'Unliked!');
+  changeLikeStatus(node_id, "/delete");
+
+  // $('#like-button-' + node_id).on('click', clickliked);
+  // // $('#like-button-' + node_id).off('click', clicknotliked);
+  // $('#like-button-' + node_id).prop('disabled', false)
+}
+
+  // toggle liked to not liked.
+  // $.getJSON("/likes/node/" + node_id + "/delete")
+  //  .done(function(response) {
+    //  window.response = response;
+    // notyNotification('mint', 3000, 'success', 'topRight', 'Unliked!');
+    // shownotliked(node_id);
+    // changelikecount(parseInt(response), node_id);
+
+// }
+
 function clickliked() {
 
   var node_id = $(this).attr('node-id');
-  // toggle liked to not liked.
-  $.getJSON("/likes/node/" + node_id + "/delete")
-   .done(function(response) {
-    notyNotification('mint', 3000, 'success', 'topRight', 'Unliked!');
-    shownotliked(node_id);
-    changelikecount(parseInt(response), node_id);
-    $('#like-button-' + node_id).on('click', clicknotliked);
-    $('#like-button-' + node_id).off('click', clickliked);
+  $('#like-button-' + node_id).prop('disabled', true);
+  $('#like-button-' + node_id).off('click', clickliked);
+  $('#like-button-' + node_id).off('click', clicknotliked);
+  changeLikeStatus(node_id, "/create");
 
-  });
-
+//   $.getJSON("/likes/node/" + node_id + "/create")
+//    .done(function(response) {
+//     notyNotification('mint', 3000, 'success', 'topRight', 'Liked!');
+//     showliked(node_id);
+//     changelikecount(parseInt(response), node_id);
+//      window.response = response;
+    // $('#like-button-' + node_id).on('click', clicknotliked);
+    // $('#like-button-' + node_id).off('click', clickliked);
+    // $('#like-button-' + node_id).prop('disabled', false)
 }
+//   });
 
-function clicknotliked() {
-
-  var node_id = $(this).attr('node-id');
-  // toggle not liked to liked.
-  $.getJSON("/likes/node/" + node_id + "/create")
-   .done(function(response) {
-    notyNotification('mint', 3000, 'success', 'topRight', 'Liked!');
-    showliked(node_id);
-    changelikecount(parseInt(response), node_id);
-    $('#like-button-' + node_id).on('click', clickliked);
-    $('#like-button-' + node_id).off('click', clicknotliked);
-
-  });
-
-}
+// }
